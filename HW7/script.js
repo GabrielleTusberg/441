6d62c4bf19434ada1a7b45a4f24160c34da860b3
Gabrielle Tusberg <gabrielle.tusberg@umontana.edu>
2018-08-23 20:26:49 -0600

    hw7
    hw7

--- HW7/script.js
+++ HW7/script.js
@@ -7,9 +7,9 @@ window.onload = function(){
 
   let variable = document.createElement ("section");
 
-  let firstheading = document.querySelector('#pick');
-  let secondheading = document.querySelector('#future');
-  let firstparagraph = document.querySelector('#summary');
+  let firstheading = document.querySelector('h1');
+  let secondheading = document.querySelector('h2');
+  let firstparagraph = document.querySelector('p');
   let buttonC = document.querySelector('#b1');
   let buttonH = document.querySelector('#b2');
   let buttonK = document.querySelector('#b3');
@@ -33,7 +33,7 @@ window.onload = function(){
   myImage3.src = "images/Image3.jpg";
 
   firstheading.innerText = "Take Your Pick";
-  secondheading.innerText = "your future awaits";
+  secondheading.innerText = "(your future awaits)";
   firstparagraph.innerText = "Everyone wants to accomplish several different things at once to better themselves in either health, knowledge, or courage. This adventure story will give you a path for each choice. There will be a consequence that follows.";
 
 
@@ -86,32 +86,23 @@ window.onload = function(){
 
   }
 
-  /*defining the button and declaring the action and function through an event listener*/
   document.querySelector("#runButton").addEventListener( 'click',grabText);
 
-  /*The next two functions are pretty much identical. They came from the lesson as well.
-  First declare and define function. Changing the text on the button. Declaring what is hidden and what is not.
-  Stylizing the background and then changing it through a timeout function. Set to 3 seconds. After the else
-  statement we set everything back to where it was at the beginning effectively giving us a return function.
-  */
+
 
   function showHideText() {
       if(textEl.hidden){
           buttonC.innerText = "Return";
           textEl.hidden = false;
-          // divEl2.hidden = false;
           formEl1.hidden = true;
           divEl1.hidden= true;
           textEl3.hidden = true;
           textEl2.hidden= true;
-          // btEl2.hidden=true;
           el.hidden=true;
 
-          // body.style.background = "#030303 url('images/lightBack.jpg') repeat right top";
-          //  setTimeout(()=>{body.style.background = "#030303 url('images/blueBack.jpg') repeat right top" ;}, 3000 );
-          //
+
       } else {
-          buttonC.innerText = "Red Button";
+          buttonC.innerText = "Courage";
           textEl.hidden = true;
           // divEl2.hidden=true;
           formEl1.hidden = false;
@@ -120,7 +111,6 @@ window.onload = function(){
           btEl2.hidden=false;
           el.hidden=false;
 
-          // body.style.background = "#030303 url('./images/darkBack.jpg') repeat right top";
       }
   }
 
@@ -128,56 +118,50 @@ window.onload = function(){
       if(textEl2.hidden){
           buttonH.innerText = "Return";
           textEl2.hidden = false;
-          imgEl1.hidden=false;
+          imgEl1.hidden= false;
           formEl1.hidden= true;
-          divEl1.hidden=true;
-          textEl3.hidden=true;
-          textEl.hidden=true;
-          btEl.hidden=true;
-          el.hidden=true;
+          divEl1.hidden= true;
+          textEl3.hidden= true;
+          textEl.hidden= true;
+          btEl.hidden= true;
+          el.hidden= true;
 
-          // body.style.background = "#030303 url('images/orangeBack.jpg') repeat right top";
-          // setTimeout(()=>{body.style.background = "#030303 url('images/knowle') repeat right top" ;}, 3000 );
 
       } else {
-          buttonH.innerText = "Blue Button";
+          buttonH.innerText = "Health";
           textEl2.hidden = true;
-          imgEl1.hidden=true;
-          formEl1.hidden=false;
+          imgEl1.hidden= true;
+          formEl1.hidden= false;
           divEl1.hidden= false;
-          textEl3.hidden=false;
-          btEl.hidden=false;
-          el.hidden=false;
-          //
-          // body.style.background = "#030303 url('images/courage.png') right top";
-      }
-      function showHideText3(){
-        if(textEl2.hidden){
-            buttonH.innerText = "Return";
-            textEl2.hidden = false;
-            imgEl1.hidden=false;
-            formEl1.hidden= true;
-            divEl1.hidden=true;
-            textEl3.hidden=true;
-            textEl.hidden=true;
-            btEl.hidden=true;
-            el.hidden=true;
+          textEl3.hidden= false;
+          btEl.hidden= false;
+          el.hidden= false;
 
-            // body.style.background = "#030303 url('images/orangeBack.jpg') repeat right top";
-            // setTimeout(()=>{body.style.background = "#030303 url('images/knowle') repeat right top" ;}, 3000 );
+      }
+    }
+      function showHideText3() {
+        if(textEl2.hidden){
+            buttonK.innerText = "Return";
+            textEl2.hidden = false;
+            imgEl1.hidden= false;
+            formEl1.hidden= true;
+            divEl1.hidden= true;
+            textEl3.hidden= true;
+            textEl.hidden= true;
+            btEl.hidden= true;
+            el.hidden= true;
+
 
         } else {
-            buttonH.innerText = "Blue Button";
+            buttonK.innerText = "Health";
             textEl2.hidden = true;
-            imgEl1.hidden=true;
-            formEl1.hidden=false;
+            imgEl1.hidden= true;
+            formEl1.hidden= false;
             divEl1.hidden= false;
-            textEl3.hidden=false;
-            btEl.hidden=false;
-            el.hidden=false;
-            //
-            // body.style.background = "#030303 url('images/courage.png') right top";
+            textEl3.hidden= false;
+            btEl.hidden= false;
+            el.hidden= false;
+
         }
       }
-  }
 }
